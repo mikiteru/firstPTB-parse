bf8000641668a8fdcf539dffac60a001c4295645
mikiteru <mikikobayashi12@gmail.com>
2020-07-04 11:22:44 +0900

    関数化しました

--- ex_nltk.py
+++ ex_nltk.py
@@ -1,10 +1,12 @@
 from nltk.tree import Tree
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import treebank
+import nltk
 #import nltk
 #nltkのライブラリは((S (...)) )ってなってるの対応してないから(S (...))の方が正しいっぽいか？
 
-#nltk.download('id')
+#nltk.download('averaged_perceptron_tagger')
+
 def ptb_read(txt):
     k = open(txt)
     sentences = k.readlines()
@@ -25,14 +27,30 @@ vp = Tree('VP',[Tree('V', ['saw'])])
     print(i)
 print(t.leaves())'''
 
-#t = Tree.fromstring("(S (NP (NNP John))(VP (VBZ loves)(NP (NNP Mary)))(. .))")
-pl=0            #単語の位置
-for i in t.subtrees():      #部分木すべてについて
-    pl = pl+t.leaves()[pl:].index(i.leaves()[0])       #単語の見る位置を部分木の初めの単語の位置に変更
-    start = pl+t.leaves()[pl:].index(i.leaves()[0])     #タグが含む範囲の初めの位置
-    end = start+len(i.leaves())                         #タグが含む範囲の終わりの位置(初めの位置+部分木の葉の数)
-    print(i.label(),start+1,end)
-t.draw()
+t = "(S (NP (NNP John))(VP (VBZ loves)(NP (NNP Mary)))(. .))"
+s = "(NP You)"
+#t = "I ate a fish and John roast beef."
+#morph = nltk.word_tokenize(t)
+#pos = nltk.pos_tag(morph)
+#print(pos)
+
+def getspan_fromtree(t: 'str of tree') \
+        -> 'span of each tag:dictionary{tag_num:(pos,start,end)})':
+    tree = Tree.fromstring(t)
+    span ={}
+    tag_num = 1
+    pl=0            #単語の位置
+    for i in tree.subtrees():      #部分木すべてについて
+        pl = pl+tree.leaves()[pl:].index(i.leaves()[0])       #単語の見る位置を部分木の初めの単語の位置に変更
+        start = pl+tree.leaves()[pl:].index(i.leaves()[0])     #タグが含む範囲の初めの位置
+        end = start+len(i.leaves())                         #タグが含む範囲の終わりの位置(初めの位置+部分木の葉の数)
+        span[tag_num] = (i.label(),start+1,end)
+        tag_num += 1
+    return(span)
+x = getspan_fromtree(t)
+print(getspan_fromtree(s))
+print(x)
+#t.draw()
 """
 a=['A','B','A','B']
 print(2+a[2:].index('B'))
